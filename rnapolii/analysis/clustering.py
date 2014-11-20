d1f99537e75d4bc24ac4665b95affebdcbe9649d
cgreenberg <cgreen@salilab.org>
2014-11-20 14:46:15 -0800

    cleaning up clustering script

--- rnapolii/analysis/clustering.py
+++ rnapolii/analysis/clustering.py
@@ -1,31 +1,27 @@
 import IMP
 import IMP.pmi
 import IMP.pmi.macros
-import sys
+import sys,os
 
-nclusters=1
+# most common settings
+nclusters = 1
+number_top_models = 5
+merge_directories = ["../modeling/"]
+prefiltervalue = 2900.0
+nclusters = 1
+out_dir = os.path.join("kmeans_",num_top_models,"_",str(nclusters))
 
-is_mpi=False
-number_of_best_scoring_models=50
-merge_directories=["../modeling/"]
-prefiltervalue=2900.0
-
-#####################
-# don't change below
-#####################
+#################################
+# should not have to change below
+##################################
 
 model=IMP.Model()
 
 # initialize the macro
-
 mc=IMP.pmi.macros.AnalysisReplicaExchange0(model,
-                                        stat_file_name_suffix="stat",     # don't change
-                                        merge_directories=["../modeling/"], # change this list splitting the runs or adding new runs
-                                        global_output_directory="./output/", # don't change
-                                        rmf_dir="rmfs/")  # don't change
+                                           merge_directories=merge_directories)
 
 # fields that have to be extracted for the stat file
-
 feature_list=["ISDCrossLinkMS_Distance_intrarb",
               "ISDCrossLinkMS_Distance_interrb",
               "ISDCrossLinkMS_Data_Score",
@@ -38,37 +34,27 @@ feature_list=["ISDCrossLinkMS_Distance_intrarb",
 # the key is the name of the file and the value if the selection
 # example:
 #              {"med17-CTD":[(200,300,"med17")],"med17-CTD.med14":[(200,300,"med17"),"med14"]   }
+density_names = {"Rpb4":("Rpb4"),
+               "Rpb7":("Rpb7")}
 
 # list of component names needed to calculate the RMSD for the clustering
+rmsd_names = {"Rpb4":"Rpb4",
+              "Rpb7":"Rpb7"}
 
-components_names=["Rpb4",
-                  "Rpb7"]
+# components used for structural alignment
+align_names = None # (None because EM provides reference frame)
 
-density_names={}
-rmsd_names={}
-for name in components_names:
-    density_names[name]=[name]
-    rmsd_names[name]=name
-
-nclusters=1                                        # number of clusters needed by kmeans
-number_of_best_scoring_models=50
-mc.clustering("SimplifiedModel_Total_Score_None",  # don't change, field where to find the score
-              "rmf_file",                          # don't change, field where to find the path for the rmf_file
-              "rmf_frame_index",                   # don't change, field for the frame index
-              prefiltervalue=2900.0,              # prefilter the models by score
-              number_of_best_scoring_models=number_of_best_scoring_models,   # number of models to be clustered
-              alignment_components=None,           # don't change, (list of proteins you want to use for structural alignment
-              rmsd_calculation_components=rmsd_names, # list of proteins used to calculated the rmsd
-              distance_matrix_file="distance.rawmatrix.pkl", # save the distance matrix
-              outputdir="kmeans_"+str(number_of_best_scoring_models)+"_"+str(nclusters)+"/",  # directory name for the clustering
-              feature_keys=feature_list,                     # extract these fields from the stat file
-              load_distance_matrix_file=False,                # skip the matrix calcuklation and read the precalculated matrix
-              skip_clustering=False,                         # skip clustering
-              display_plot=False,                            # display the heat map plot of the distance matrix
-              exit_after_display=False,                      # exit after having displayed the distance matrix plot
-              get_every=1,                                   # skip structures for faster computation
-              is_mpi=is_mpi,                                 # mpi enabled
-              number_of_clusters=nclusters,                  # number of clusters to be used by kmeans algorithm
-              voxel_size=3.0,                                # voxel size of the mrc files
-              density_custom_ranges=density_names)    # setup the list of densities to be calculated
-
+mc.clustering(prefiltervalue=prefiltervalue                    # prefilter the models by score
+              number_of_best_scoring_models=num_top_models,    # number of models to be clustered
+              alignment_components=None,                       # list of proteins you want to use for structural alignment
+              rmsd_calculation_components=rmsd_names,          # list of proteins used to calculated the rmsd
+              distance_matrix_file="distance.rawmatrix.pkl",   # save the distance matrix
+              outputdir=out_dir                                # location for clustering results
+              feature_keys=feature_list,                       # extract these fields from the stat file
+              load_distance_matrix_file=False,                 # skip the matrix calcuklation and read the precalculated matrix
+              display_plot=False,                              # display the heat map plot of the distance matrix
+              exit_after_display=False,                        # exit after having displayed the distance matrix plot
+              get_every=1,                                     # skip structures for faster computation
+              number_of_clusters=nclusters,                    # number of clusters to be used by kmeans algorithm
+              voxel_size=3.0,                                  # voxel size of the mrc files
+              density_custom_ranges=density_names)             # setup the list of densities to be calculated
