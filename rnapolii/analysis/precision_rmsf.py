1d8c7dfd2ce2e15a3bf1c9422fde641172afacc4
cgreenberg <cgreen@salilab.org>
2014-11-21 10:39:16 -0800

    making precision work with new interfaces

--- rnapolii/analysis/precision_rmsf.py
+++ rnapolii/analysis/precision_rmsf.py
@@ -8,12 +8,12 @@ import itertools
 
 # common settings
 test_mode = False                        # runs on every 10 frames
-root_cluster_directory = 'kmeans_5_1'  # specify the directory (of clusters) to be analysed
+root_cluster_directory = 'kmeans_5_1'    # specify the directory (of clusters) to be analysed
 
 
 # choose components for the precision calculation
 # key is the named precision item
-# value is a list of components
+# value is a list of selection tuples
 selections={"Rpb4":["Rpb4"],
             "Rpb7":["Rpb7"],
             "Rpb4_Rpb7":["Rpb4","Rpb7"]}
@@ -25,39 +25,38 @@ selections={"Rpb4":["Rpb4"],
 
 # setup Precision calculator
 model = IMP.Model()
-pr = IMP.pmi.analysis.Precision(model,'one',selection_dictionary=selections)
+pr = IMP.pmi.analysis.Precision(model,resolution=1,selection_dictionary=selections)
 pr.set_precision_style('pairwise_rmsd')
 
 
 # gather the RMF filenames for each cluster
-rmfs=[]
-frames=[]
+rmf_list=[]
+frame_list=[]
 cluster_dirs=glob.glob(root_cluster_directory+'/cluster.*/')
 if test_mode:
   # runs on the first 10 structures to test if it runs smoothly
   for d in cluster_dirs:
-      rmfs.append(glob.glob(d+'/*.rmf3')[0::10])
-      frames.append([0]*len(rmfs[-1]))
+      rmf_list.append(glob.glob(d+'/*.rmf3')[0::10])
+      frame_list.append([0]*len(rmf_list[-1]))
 else:
   for d in cluster_dirs:
-      rmfs.append(glob.glob(d+'/*.rmf3')[0::1])
-      frames.append([0]*len(rmfs[-1]))
+      rmf_list.append(glob.glob(d+'/*.rmf3')[0::1])
+      frame_list.append([0]*len(rmf_list[-1]))
 
 # add them to the Precision object
-for n in range(len(rmfs)):
-    pr.add_structures(zip(rmfs[n],frames[n]),cluster_dirs[n])
+for rmfs,frames,cdir in zip(rmf_list,frame_list,cluster_dirs):
+    pr.add_structures(zip(rmfs,frames),cdir)
 
 # calculate intra-cluster and inter-cluster precision
 print "calculating precision"
-for clus1,clus2 in itertools.combinations_with_replacement(range(len(rmfs)),2):
-    pr.get_precision(root_cluster_directory+"/precision."+str(clus1)+"."+str(clus2)+".out",
-                     cluster_dirs[clus1],
+for clus1,clus2 in itertools.combinations_with_replacement(range(len(rmf_list)),2):
+    pr.get_precision(cluster_dirs[clus1],
                      cluster_dirs[clus2],
-                     skip=1)
+                     root_cluster_directory+"/precision."+str(clus1)+"."+str(clus2)+".out")
 
 # compute resiude mean-square fluctuation (RMSF)
 print "calculating RMSF"
-for n in range(len(rmfs)):
-    pr.get_rmsf(cluster_dirs[n],cluster_dirs[n]+"/",skip=1)
+for d in cluster_dirs:
+    pr.get_rmsf(structure_set_name=d,outdir=d)
 
 print "done"
