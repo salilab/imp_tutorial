a0cddf11d6cff39540d6ce5cef45b3958369fd57
Ben Webb <ben@salilab.org>
2022-07-14 15:42:35 -0700

    Drop workaround for Python 2.6
    IMP doesn't work with Python 2.6 any more,
    so we can require 2.7 too.

--- rnapolii/analysis/precision_rmsf.py
+++ rnapolii/analysis/precision_rmsf.py
@@ -14,27 +14,8 @@ import IMP.pmi.output
 import IMP.atom
 import glob
 from copy import deepcopy
-try:
-    from itertools import combinations_with_replacement
-except ImportError:
-    # Provide implementation (from Python's own itertools docs)
-    # for Python < 2.7
-    def combinations_with_replacement(iterable, r):
-        # combinations_with_replacement('ABC', 2) --> AA AB AC BB BC CC
-        pool = tuple(iterable)
-        n = len(pool)
-        if not n and r:
-            return
-        indices = [0] * r
-        yield tuple(pool[i] for i in indices)
-        while True:
-            for i in reversed(range(r)):
-                if indices[i] != n - 1:
-                    break
-            else:
-                return
-            indices[i:] = [indices[i] + 1] * (r - i)
-            yield tuple(pool[i] for i in indices)
+from itertools import combinations_with_replacement
+
 
 # common settings
 test_mode = False                           # runs on every 20 frames
